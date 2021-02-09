0abe19aab43c005617e80a0d356661d5a3661d6d
Will <williamjamescook@aol.com>
2021-02-09 14:50:55 +0000

    created topics path

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Title from "./components/Title";
 import Nav from "./components/NavBar";
 import ArticlesList from "./components/ArticlesList";
+import { Router } from "@reach/router";
 
 function App() {
 	return (
@@ -9,7 +10,10 @@ function App() {
 			<header className="App-header">
 				<Title />
 				<Nav />
-				<ArticlesList />
+				<Router>
+					<ArticlesList path="/" />
+					<ArticlesList path="/topic/:topic" />
+				</Router>
 			</header>
 		</div>
 	);
