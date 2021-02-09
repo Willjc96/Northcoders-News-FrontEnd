4244de5e1aa3ae4bcee4d593ab8ac579db5423ba
Will <williamjamescook@aol.com>
2021-02-09 12:52:09 +0000

    imported components into app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
+import Title from "./components/Title";
+import Nav from "./components/NavBar";
+import ArticlesList from "./components/ArticlesList";
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<h1>Nc-News</h1>
+				<Title />
+				<Nav />
+				<ArticlesList />
 			</header>
 		</div>
 	);
