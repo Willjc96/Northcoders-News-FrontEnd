0fbe80244ad63bfda1ae3d64ac5d65183440e14e
Will <williamjamescook@aol.com>
2021-02-09 14:23:03 +0000

    created fetchArticles to import data from api.js

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,23 @@
 import React, { Component } from "react";
+import * as api from "../api";
 
 class NavBar extends Component {
+	state = {
+		articles: [],
+	};
+
+	componentDidMount() {
+		this.fetchArticles();
+	}
+
 	render() {
 		return <nav className="navbar">NavBar</nav>;
+	}
+
+	fetchArticles() {
+		api.getArticles().then((articles) => {
+			this.setState({ articles });
+		});
 	}
 }
 
